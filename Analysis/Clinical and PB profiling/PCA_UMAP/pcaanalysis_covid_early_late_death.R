7a1971b704192b1ee6f6c9d1fa7b21e202e3d402
Dr Joao Luiz <72543547+joaolsf@users.noreply.github.com>
2024-04-04 14:07:46 +0100

    Update pcaanalysis_covid_early_late_death.R

--- Analysis/Clinical and PB profiling/PCA_UMAP/pcaanalysis_covid_early_late_death.R
+++ Analysis/Clinical and PB profiling/PCA_UMAP/pcaanalysis_covid_early_late_death.R
@@ -1,7 +1,5 @@
-# reading file (importar o arquivo)
-multiplex <- read.csv("COVID_Clinical_Luminex_early_late_death.csv")
 
-# data frame para cluster
+multiplex <- read.csv("COVID_Clinical_Luminex_early_late_death.csv")
 df_to_cluster <- multiplex[,-1]
 rownames(df_to_cluster) <- multiplex$RecordID
 
@@ -80,138 +78,12 @@ res.desc$Dim.1
 # Description of dimension 1
 res.desc$Dim.2
 
-#quality and contribution individuals
-#it’s also possible to color individuals by their cos2 values:
-fviz_pca_ind(pca.mydata, col.ind = "cos2", 
-               gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-               repel = TRUE, axes.linetype = "blank") + theme_bw(base_size = 14)
-#You can also show the contrib of the corresponding individuals:
-fviz_pca_ind(pca.mydata, col.ind = "contrib", 
-             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE, axes.linetype = "blank") + theme_bw(base_size = 14)
-#You can also change the point size according the cos2 and colour based on contribution of the corresponding individuals:
-fviz_pca_ind(pca.mydata, geom.ind = "point", col.ind = "contrib", pointsize = "cos2", 
-             pointshape = 19, gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
-             repel = TRUE, axes.linetype = "blank") + theme_bw(base_size = 14)
-
-# how to color individuals by groups
-fviz_pca_ind(pca.mydata,
-             geom.ind = "point", # show points only (nbut not "text")
-             col.ind = cats, # color by groups
-             palette = c("black","grey", "#00AFBB", "#E7B800", "#FC4E07"),
-             addEllipses = TRUE, # Concentration ellipses
-             legend.title = "Groups")
-#If you want confidence ellipses instead of concentration ellipses, use ellipse.type = “confidence”.
-p <- fviz_pca_ind(pca.mydata,
-             geom.ind = "point", # show points only (nbut not "text")
-             col.ind = cats3, # color by groups
-             palette = c("orange", "black","grey", "#00AFBB", "#E7B800", "#FC4E07"),
-             addEllipses = FALSE, ellipse.type = "confidence", ellipse.level = 0.95,
-             legend.title = "Groups", pointshape = 19, mean.point = FALSE, axes.linetype = "blank")
-
-p + geom_point(aes(colour = factor(cats3)), size = 5) + ggtitle("PC Patients") + theme_bw(base_size = 14) 
-
-#paletes examples
-c("black","#00AFBB", "#E7B800", "#FC4E07")
-"npg"
-"ggsci"
-#Size and shape of plot elements
-#Type ggpubr::show_point_shapes() to see available point shapes.
-ggpubr::show_point_shapes() 
-#The argument axes.linetype can be used to specify the line type of axes. Default is “dashed”. 
-#Allowed values include “blank”, “solid”, “dotted”, etc. To see all possible values type ggpubr::show_line_types() in R.
-ggpubr::show_line_types()
-
-#To specify supplementary individuals and variables and qualitative variables, the function PCA() can be used as follow:
-pca.mydata2 <- PCA(df_to_cluster, quali.sup = 33, graph=FALSE)
-
-p <- fviz_pca_ind(pca.mydata2, col.ind.sup = "blue", repel = TRUE)
-p <- fviz_add(p, pca.mydata2$quali.sup$coord, color = "red")
-p
-#To plot the graph with 3 clusters
-fviz_pca_ind(pca.mydata2, geom.ind = "point", habillage = 33, pointsize = 5, pointshape = 19,
-             addEllipses =FALSE, ellipse.type = "confidence", ellipse.level = 0.65,
-             palette = c("grey", "brown", "black"), repel = TRUE, mean.point = FALSE, axes.linetype = "blank") + theme_bw(base_size = 14)
-
-#To plot the graph with H&E patterns
-fviz_pca_ind(pca.mydata2, geom.ind = "point", habillage = 31, pointsize = 10, pointshape = 19,
-             addEllipses =FALSE, ellipse.type = "confidence", ellipse.level = 0.95,
-             palette = c("grey", "brown", "black", "blue","red"), repel = TRUE, mean.point = FALSE, axes.linetype = "blank") + theme_bw(base_size = 14)
-
-#To plot the graph with Spike lung patterns
-fviz_pca_ind(pca.mydata2, geom.ind = "point", habillage = 32, pointsize = 10, pointshape = 19,
-             addEllipses =FALSE, ellipse.type = "confidence", ellipse.level = 0.95,
-             palette = c("grey", "brown", "black"), repel = TRUE, mean.point = FALSE, axes.linetype = "blank") + theme_bw(base_size = 14)
-
-#To plot the graph with ISH patterns
-fviz_pca_ind(pca.mydata2, geom.ind = "point", habillage = 33, pointsize = 10, pointshape = 19,
-             addEllipses =FALSE, ellipse.type = "confidence", ellipse.level = 0.95,
-             palette = c("grey", "brown", "black", "blue"), repel = TRUE, mean.point = FALSE, axes.linetype = "blank") + theme_bw(base_size = 14)
-
-#To plot the graph with MAC387 patterns
-fviz_pca_ind(pca.mydata2, geom.ind = "point", habillage = 34, pointsize = 10, pointshape = 19,
-             addEllipses =FALSE, ellipse.type = "confidence", ellipse.level = 0.95,
-             palette = c("blue", "brown", "black", "grey"), repel = TRUE, mean.point = FALSE, axes.linetype = "blank") + theme_bw(base_size = 14)
-
-#To plot the graph with CD3 patterns
-fviz_pca_ind(pca.mydata2, geom.ind = "point", habillage = 35, pointsize = 10, pointshape = 19,
-             addEllipses =FALSE, ellipse.type = "confidence", ellipse.level = 0.95,
-             palette = c("blue", "brown", "black", "grey"), repel = TRUE, mean.point = FALSE, axes.linetype = "blank") + theme_bw(base_size = 14)
-
-#To plot the graph with CD20 patterns
-fviz_pca_ind(pca.mydata2, geom.ind = "point", habillage = 36, pointsize = 10, pointshape = 19,
-             addEllipses =FALSE, ellipse.type = "confidence", ellipse.level = 0.95,
-             palette = c("blue", "brown", "black", "grey", "red"), repel = TRUE, mean.point = FALSE, axes.linetype = "blank") + theme_bw(base_size = 14)
-
-#To plot the graph with Bronchial patterns
-fviz_pca_ind(pca.mydata2, geom.ind = "point", habillage = 37, pointsize = 10, pointshape = 19,
-             addEllipses =FALSE, ellipse.type = "confidence", ellipse.level = 0.95,
-             palette = c("grey", "brown", "black"), repel = TRUE, mean.point = FALSE, axes.linetype = "blank") + theme_bw(base_size = 14)
-
-#To plot the graph with Alveolar patterns
-fviz_pca_ind(pca.mydata2, geom.ind = "point", habillage = 38, pointsize = 10, pointshape = 19,
-             addEllipses =FALSE, ellipse.type = "confidence", ellipse.level = 0.95,
-             palette = c("grey", "brown", "black"), repel = TRUE, mean.point = FALSE, axes.linetype = "blank") + theme_bw(base_size = 14)
-
-#To plot the graph with Interstitial patterns
-fviz_pca_ind(pca.mydata2, geom.ind = "point", habillage = 39, pointsize = 10, pointshape = 19,
-             addEllipses =FALSE, ellipse.type = "confidence", ellipse.level = 0.95,
-             palette = c("grey", "brown", "black"), repel = TRUE, mean.point = FALSE, axes.linetype = "blank") + theme_bw(base_size = 14)
-
-#To plot the graph with Macrophage clusters
-fviz_pca_ind(pca.mydata2, geom.ind = "point", habillage = 40, pointsize = 10, pointshape = 19,
-             addEllipses =FALSE, ellipse.type = "confidence", ellipse.level = 0.95,
-             palette = c("red", "brown", "black", "blue", "grey"), repel = TRUE, mean.point = FALSE, axes.linetype = "blank") + theme_bw(base_size = 14)
-
-
-
-
-
-"#00BA38", "#619CFF", "#F8766D"
-
-#All the outputs of the PCA (individuals/variables coordinates, contributions, etc) can be exported at once, into a TXT/CSV file, using the function write.infile() [in FactoMineR] package:
-# Export into a TXT file
-write.infile(pca.mydata, "pca.txt", sep = "\t")
-# Export into a CSV file
-write.infile(pca.mydata, "pca.csv", sep = ";")
-
-
-
 #Biplot
-
 pca.mydata3 <- PCA(df_to_cluster, quali.sup = 96:98, graph=FALSE)
-
 fviz_pca_biplot(pca.mydata,
                 col.ind = "black",  # Individuals color
                 col.var = "contrib", 
 repel = TRUE, alpha.var = "contrib", gradient.cols = viridis(3, direction = -1)) + theme_bw(base_size = 12)
-
-#col.ind = "#696969",  # Individuals color
-#col.var = "#2E9FDF", # Variables color
-#to color both individuals by clusters or k-means clusters and variables by contribution.
-options(ggrepel.max.overlaps = 50)
-display.brewer.all(n=10, exact.n=FALSE)
-
 p <- fviz_pca_biplot(pca.mydata,
                 # Fill individuals by groups
                 geom.ind = "point",
@@ -226,8 +98,6 @@ p <- fviz_pca_biplot(pca.mydata,
   ggpubr::fill_palette(palette = magma(11, alpha = 1, begin = 0, end = 0.95, direction = -1)) + theme_light(base_size = 12)  # Indiviual fill color
 
 p + geom_point(aes(shape = multiplex$Category), size=6) + scale_shape_manual(values=c(21, 22)) #changes the shape accordingly to the groups 
-
-
 
 #ploting PCA only with the individuals data (not showing the  variables)
 p2 <- fviz_pca_ind(pca.mydata,
